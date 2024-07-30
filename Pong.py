b787128f059ad34b68bc35727b08948ba4ed91cc
Manzi Fabrice Niyigaba <163777783+Manzi-Fabrice@users.noreply.github.com>
2024-07-30 16:57:32 -0400

    added the Ai controlled paddles

--- Pong.py
+++ Pong.py
@@ -1,13 +1,12 @@
-# Purpose: Python code for creating pong game
-
+# Purpose: Python code for creating pong game with enhanced Q-learning AI
 
 from cs1lib import *
+import random
+import numpy as np
 
 # Keys for the movement of paddle
 MOVE_UP_LEFT_PADDLE = 'a'
 MOVE_DOWN_LEFT_PADDLE = 'z'
-MOVE_UP_RIGHT_PADDLE = 'k'
-MOVE_DOWN_RIGHT_PADDLE = 'm'
 
 # Keys for directing the game
 QUIT = 'q'
@@ -17,9 +16,7 @@ START = ' '
 RADIUS = 15
 
 # window size
-# since I consider my window as square the height and the width of the window are equal to window size
-WIN_SIZE = 400
-
+WIN_SIZE = 500
 
 # width and height of the paddles
 WIDTH = 20
@@ -28,6 +25,9 @@ HEIGHT = 80
 # movement in pixel of paddle when the key is pressed
 MOVEMENT = 20
 
+# movement in pixel for AI
+MOVEMENT_AI = 15
+
 # the x_coordinates of the paddles
 XLEFT = 0
 XRIGHT = WIN_SIZE - WIDTH
@@ -35,13 +35,11 @@ XRIGHT = WIN_SIZE - WIDTH
 # setting the value of key pressed to False
 a_pressed = False
 z_pressed = False
-k_pressed = False
-m_pressed = False
 
 # Other variables
 # the y_coordinates of the paddles
 ya_left = 0
-ya_right = WIN_SIZE - HEIGHT
+ya_right = WIN_SIZE // 2 - HEIGHT // 2
 
 # start
 start_pong = False
@@ -58,30 +56,94 @@ BALL_SPEED = 15
 vx = 0
 vy = 0
 
+# Q-learning parameters
+q_table = {}  # Initialize Q-table
+alpha = 0.1  # Learning rate
+gamma = 0.9  # Discount factor
+epsilon = 0.1  # Exploration rate
+epsilon_decay = 0.995  # Decay rate for epsilon
+
+# Performance metrics
+successful_hits = 0
+total_games = 0
+wins = 0
+game_durations = []
+hits_per_game = []
+
+# Current game metrics
+current_game_duration = 0
+current_game_hits = 0
+
+
+def get_state():
+    return (int(ball_x), int(ball_y), int(ya_right))
+
+
+def choose_action(state):
+    if random.random() < epsilon:  # Exploration
+        return random.choice(['up', 'down'])
+    else:  # Exploitation
+        if state not in q_table:
+            q_table[state] = {'up': 0, 'down': 0}
+        return max(q_table[state].items(), key=lambda x: x[1])[0]
+
+
+def update_q_table(state, action, reward, next_state):
+    if state not in q_table:
+        q_table[state] = {'up': 0, 'down': 0}
+    if next_state not in q_table:
+        q_table[next_state] = {'up': 0, 'down': 0}
+    q_table[state][action] = q_table[state][action] + alpha * (
+            reward + gamma * max(q_table[next_state].values()) - q_table[state][action]
+    )
+
+
+def ai_control_paddle():
+    global ya_right, q_table, epsilon, successful_hits, current_game_hits
+    state = get_state()  # Define game state
+    action = choose_action(state)  # Choose action using Q-table
+    reward = 0  # Initialize reward
+
+    # Check ball's direction and move paddle accordingly
+    if ball_y < ya_right + HEIGHT // 2 and ya_right > 0:
+        ya_right -= MOVEMENT_AI
+    elif ball_y > ya_right + HEIGHT // 2 and ya_right < WIN_SIZE - HEIGHT:
+        ya_right += MOVEMENT_AI
+
+    next_state = get_state()
+
+    # Rewards and penalties
+    if ball_x + RADIUS >= XRIGHT and ya_right <= ball_y <= ya_right + HEIGHT:
+        reward = 1  # Reward for hitting the ball
+        successful_hits += 1
+        current_game_hits += 1
+    elif ball_x + RADIUS >= XRIGHT:
+        reward = -1  # Penalty for missing the ball
+
+    reward += 0.01  # Small reward for keeping the ball in play
+
+    update_q_table(state, action, reward, next_state)
+    epsilon = max(epsilon * epsilon_decay, 0.01)  # Decay epsilon
+
 
 # function that will draw the paddles
 def drawing_paddles():
-    # setting the color of paddles to blue
-    set_fill_color(0, 0, 1)
-
-    # drawing the right paddle
-    draw_rectangle(XRIGHT, ya_right, WIDTH, HEIGHT)
-
-    # drawing the left paddle
-    draw_rectangle(XLEFT, ya_left, WIDTH, HEIGHT)
+    set_fill_color(0, 0, 1)  # Blue color for paddles
+    draw_rectangle(XRIGHT, ya_right, WIDTH, HEIGHT)  # Right paddle
+    draw_rectangle(XLEFT, ya_left, WIDTH, HEIGHT)  # Left paddle
 
 
 # function that draws the ball
 def draw_ball():
-    set_fill_color(0, 1, 0)
+    set_fill_color(0, 1, 0)  # Green color for ball
     draw_circle(ball_x, ball_y, RADIUS)
 
 
-# function that update the ball function
+# function that update the ball position
 def updated_position():
     global ball_y, ball_x
-    ball_y = ball_y + vy
-    ball_x = ball_x + vx
+    ball_y += vy
+    ball_x += vx
 
 
 # checking the collision with the ceiling or the floor
@@ -93,20 +155,28 @@ def check_horizontal_collision():
 
 # this function will reset the game
 def reset_game():
-    global ball_x, ball_y, end_game, start_pong, ya_right, ya_left
+    global ball_x, ball_y, end_game, start_pong, ya_right, ya_left, epsilon, total_games, wins, current_game_duration, current_game_hits
     ball_x = WIN_SIZE // 2
     ball_y = WIN_SIZE // 2
     ya_left = 0
-    ya_right = WIN_SIZE - HEIGHT
+    ya_right = WIN_SIZE // 2 - HEIGHT // 2
     start_pong = False
     end_game = False
+    epsilon = 0.1  # Reset epsilon
+    total_games += 1
+    game_durations.append(current_game_duration)
+    hits_per_game.append(current_game_hits)
+    current_game_duration = 0
+    current_game_hits = 0
 
 
 # this function will check wall collision and end the game
 def wall_collision():
-    global end_game
-    if ball_x - RADIUS <= 0 or ball_x + RADIUS >= WIN_SIZE:
-        end_game = True
+    global end_game, wins
+    if ball_x - RADIUS <= 0:
+        wins += 1
+        reset_game()
+    elif ball_x + RADIUS >= WIN_SIZE:
         reset_game()
 
 
@@ -121,18 +191,14 @@ def check_paddle_collision():
         vx = -BALL_SPEED
 
 
-# this function detect they key pressed and change global key pressed to True
+# this function detect the key pressed and change global key pressed to True
 def key_pressed(key):
-    global ya_left, ya_right, a_pressed, k_pressed, m_pressed, z_pressed, vx, vy, start_pong, terminate
+    global ya_left, a_pressed, z_pressed, vx, vy, start_pong, terminate
 
     if key == MOVE_UP_LEFT_PADDLE:
         a_pressed = True
     if key == MOVE_DOWN_LEFT_PADDLE:
         z_pressed = True
-    if key == MOVE_DOWN_RIGHT_PADDLE:
-        m_pressed = True
-    if key == MOVE_UP_RIGHT_PADDLE:
-        k_pressed = True
 
     # starting the game and checking the flag of start pong to ensure that the space bar has effect only one time
     if key == START and not start_pong:
@@ -147,20 +213,16 @@ def key_pressed(key):
 
 # This function detect the key release and set the global key pressed to false
 def key_released(key):
-    global ya_left, ya_right, a_pressed, k_pressed, m_pressed, z_pressed
+    global ya_left, a_pressed, z_pressed
 
     if key == MOVE_UP_LEFT_PADDLE:
         a_pressed = False
     if key == MOVE_DOWN_LEFT_PADDLE:
         z_pressed = False
-    if key == MOVE_DOWN_RIGHT_PADDLE:
-        m_pressed = False
-    if key == MOVE_UP_RIGHT_PADDLE:
-        k_pressed = False
 
 
 def main_draw():
-    global start_pong, end_game, ya_left, ya_right, vx, vy
+    global start_pong, end_game, ya_left, vx, vy, current_game_duration
 
     # setting the background to black
     set_clear_color(0, 0, 0)
@@ -173,10 +235,9 @@ def main_draw():
         ya_left += MOVEMENT
     if a_pressed and (ya_left > 0):
         ya_left -= MOVEMENT
-    if m_pressed and (ya_right < WIN_SIZE - HEIGHT):
-        ya_right += MOVEMENT
-    if k_pressed and (ya_right > 0):
-        ya_right -= MOVEMENT
+
+    # AI control for the right paddle
+    ai_control_paddle()
 
     if not end_game:
         drawing_paddles()
@@ -187,6 +248,7 @@ def main_draw():
         check_paddle_collision()
         wall_collision()
         updated_position()
+        current_game_duration += 1
+
 
 start_graphics(main_draw, height=WIN_SIZE, width=WIN_SIZE, key_press=key_pressed, key_release=key_released)
-
